71d09891b6c79c5949d7cadc730a332a3416d4cf
LuvasHenri <lukhenri29@gmail.com>
2024-07-23 20:41:36 -0300

    sistema de busca

--- search.php
+++ search.php
@@ -5,18 +5,97 @@ require_once("./conexao/conexao.php");
 try {
 
     $buscaValue = isset($_GET['search']) ? $_GET['search'] : '';
-    $sql = "SELECT * FROM estoque WHERE armazenamento = :value";
-    $comandoSQL = $conexao->prepare($sql);
-    $comandoSQL->execute(array(':value' => $buscaValue));
-    $resultado = $comandoSQL->fetch();
+    $comandoSQL = "SELECT * FROM estoque WHERE armazenamento = :value";
+    $SQL_prepare = $conexao->prepare($comandoSQL);
+    $SQL_prepare->execute(array(':value' => $buscaValue));
+    $dados = $SQL_prepare->fetchAll();
 
-    if ($resultado) {
-        print_r($resultado);
+    $totalRegistros = $SQL_prepare->rowCount();
+    if ($dados) {
+        
     } else {
         echo "*".htmlspecialchars($buscaValue)."*" ."  não encontrado(a)";
     }
-
+    
 } catch (Throwable $th) {
    // echo "Erro: " . $th->getMessage();
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="PT-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/estilovizu.css">
+    <title>ESTOQUE</title>
+</head>
+
+<body>
+    
+        </div>
+        <div class="container">
+            <table>
+            
+            <thead>
+                <tr>
+                    <!--<th >cod <br> prod</th>-->
+                    <th>NOME</th>
+                    <th>QUANTIDADE</th>
+                    <th>CONTROLE</th>
+                    <th>EX</th>
+
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+
+                    if ($totalRegistros > 0) {
+                    foreach ($dados as $linha) {
+
+                    ?>
+                    <tr 
+                    <?php 
+
+                    if ($linha["quantidadeEstoque"] <= $linha["quantidademinimaEstoque"] ){
+                        echo 'class="qtdMin"';
+                    }
+                    
+                    ?>>
+                    <!--<td text align="center"><?= $linha["IDEstoque"]; ?></td>-->
+                    <td><?= $linha["nomeEstoque"]; ?></td>
+                    <td text align="center"><?= $linha["quantidadeEstoque"]; ?></td>
+                    <td text align="center"><a href="./editar.php?id=<?= $linha['IDEstoque']; ?>">
+                    EDITAR
+                    </a></td> 
+                    <td text align="center" ><a href="./excluir.php?id=<?= $linha['IDEstoque']; ?>">X</td>  
+                    </tr>
+                    
+                    <?php
+                    }
+                    }
+                    ?>
+
+</tbody>
+    <div class="search">
+       
+        <form action="./search.php" method="GET">
+            
+            <input type="text" name="search" value="" width="100PX">
+            <a href="./search2.php"><button type="$_POST">BUSCAR</button></a>
+
+        </form>
+        <button class="bnt-voltar-estoque">
+            <a href="index.php">INICIO</a>
+        </button>
+    </div>
+
+
+</table>
+</div>
+
+
+</body>
+
+</html>
