734eca4cbb305f420bc5f05e6d3288169f053d75
k4rambit <lucafsrocha@hotmail.com>
2024-08-15 21:02:06 -0300

    att cad users

--- users/menu.php
+++
@@ -1,7 +0,0 @@
-    <div class="menu">
-        <ul>
-            <a href="./cadastro.php"><li>CADASTRAR</li></a>
-            <a href="./visualizacao.php"><li>VISUALIZAR</li></a>
-            <a href="../index.php"><li>SAIR</li></a>
-        </ul>
-    </div>
