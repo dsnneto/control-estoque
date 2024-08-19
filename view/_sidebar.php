3b067452bf963d9a3e00823258fa35af1ec5a7c0
k4rambit <lucafsrocha@hotmail.com>
2024-08-19 20:36:39 -0300

    update

--- view/_sidebar.php
+++ view/_sidebar.php
@@ -1,14 +1,16 @@
-<div class="sidebar">
+    <div class="sidebar">
+        <div class="sidebar-info">
             <h3>Home</h3>
             <a href="#"><i class="fa-solid fa-table-cells-large"></i>Dashboard</a>
-            <a href="./vis-estoque.php"><i class="fa-solid fa-box"></i>Estoque</a>
-            <a href="#"><i class="fa-solid fa-table-cells-large"></i>Dashboard</a>
-            <a href="#"><i class="fa-solid fa-table-cells-large"></i>Dashboard</a>
-            <hr>
-            <br>
+            <a href="./estoque-view.php"><i class="fa-solid fa-box"></i>Estoque</a>
+            <a href="#"><i class="fa-solid fa-clipboard"></i>Relatórios</a>
+            <a href="#"><i class="fa-solid fa-user"></i>Usuários</a>
+        </div>
+        <div class="sidebar-info">
             <h3>Home</h3>
             <a href="#"><i class="fa-solid fa-table-cells-large"></i>Dashboard</a>
             <a href="#"><i class="fa-solid fa-table-cells-large"></i>Dashboard</a>
             <a href="#"><i class="fa-solid fa-table-cells-large"></i>Dashboard</a>
             <a href="#"><i class="fa-solid fa-table-cells-large"></i>Dashboard</a>
         </div>
+    </div>
